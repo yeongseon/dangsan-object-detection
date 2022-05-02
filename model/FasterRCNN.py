7b3af78f4df6c94a9ca152330ca8fd607267ef88
jh.park <jh.park@neowiz.com>
2022-05-02 20:53:40 +0900

    Write model comments

--- model/FasterRCNN.py
+++ model/FasterRCNN.py
@@ -3,6 +3,13 @@ import torch.nn as nn
 import torchvision.models as models
 
 class FasterRCNN(nn.Module):
+    """
+    Faster R-CNN: Towards Real-Time Object Detection with Region Proposal Networks
+    https://arxiv.org/abs/1506.01497
+    
+    Pre-trained
+    https://pytorch.org/vision/main/generated/torchvision.models.detection.fasterrcnn_resnet50_fpn.html#torchvision.models.detection.fasterrcnn_resnet50_fpn
+    """
     def __init__(self,
                  nclasses: int = 2,
                  pretrained=False,
@@ -11,10 +18,17 @@ class FasterRCNN(nn.Module):
 
         model = models.detection.fasterrcnn_resnet50_fpn(
             pretrained=pretrained, pretrained_backbone=pretrained_backbone)
-
+        #
+        # Faster r-cnn with ResNet-50
+        # pretrained(bool): If True, returns a model pre-trained on COCO train2017
+        # pretrained_backbone(bool): If True, returns a model with backbone pre-trained on Imagenet
+        
         in_features = model.roi_heads.box_predictor.cls_score.in_features
         model.roi_heads.box_predictor = models.detection.faster_rcnn.FastRCNNPredictor(
             in_features, nclasses)
+        # in_channel (int) : number of input channels
+        # num_classes (int) : number of output classes
+        # return classification, bounding box regression
 
         self.model = model
 
