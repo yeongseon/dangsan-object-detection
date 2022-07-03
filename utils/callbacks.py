b53f6a180b955cf96f72345aa77e483cabd07d96
이봉호(Bongho, Lee) <bongho@hanyang.ac.kr>
2022-07-03 18:35:11 +0900

    COCOMETRICRESULT 삭제

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -22,8 +22,8 @@ class EarlyStopping():
         assert self.monitor in metrics, f"{self.monitor} not a metric"
         value = metrics[self.monitor]
         
-        if type(value).__name__ == "COCOMetricResults" :
-          value = value['map_50']
+        # if type(value).__name__ == "COCOMetricResults" :
+        #   value = value['map_50']
         
         if self.mode == "max":
             if value < self.value_keep:
@@ -108,8 +108,8 @@ class ModelCheckpoint():
 
         value = self._check_metric_name(metrics)
         
-        if type(value).__name__ == "COCOMetricResults" :
-          value = value['map_50']
+        # if type(value).__name__ == "COCOMetricResults" :
+        #   value = value['map_50']
           
         if self.mode == "max":
             if value >= self.value_keep:
